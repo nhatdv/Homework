eeff8bd49999ff8df0b57c00878790da12b9ba69
nhatdv <SCN@localhost.(none)>
2013-10-16 00:48:42 +0700

    commit zigzag example day 06

---
+++ Zigzag/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Zigzag
+//
+//  Created by MAC on 10/15/13.
+//  Copyright (c) 2013 MAC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
