08fef2e7f5eb074f7c93db20610778a3dbbf4677
nhatdv <nhatdv@gmail.com>
2013-10-27 21:14:57 +0700

    Day09

---
+++ Day09/Day09/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Day09
+//
+//  Created by MAC on 10/27/13.
+//  Copyright (c) 2013 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
